af44374d875c579f3d5ec18fb7452db64bfb79c9
Lane Kolbly <lane@rscheme.org>
2026-01-23 18:41:45 -0600

    Serial USB

--- fw/src/main.rs
+++ fw/src/main.rs
@@ -3,26 +3,138 @@
 
 mod fmt;
 
+use embassy_usb::{
+    Builder,
+    class::cdc_acm::{CdcAcmClass, State},
+    driver::EndpointError,
+};
 #[cfg(not(feature = "defmt"))]
 use panic_halt as _;
 #[cfg(feature = "defmt")]
 use {defmt_rtt as _, panic_probe as _};
 
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_futures::join::{join, join3};
+use embassy_stm32::{
+    bind_interrupts,
+    gpio::{Level, Output, Speed},
+    peripherals,
+    usb::{self, Driver, Instance},
+};
 use embassy_time::{Duration, Timer};
 use fmt::info;
 
+bind_interrupts!(struct Irqs {
+    USB => usb::InterruptHandler<peripherals::USB>;
+});
+
+struct Disconnected {}
+
+impl From<EndpointError> for Disconnected {
+    fn from(val: EndpointError) -> Self {
+        match val {
+            EndpointError::BufferOverflow => panic!("Buffer overflow"),
+            EndpointError::Disabled => Disconnected {},
+        }
+    }
+}
+
+async fn echo<'d, T: Instance + 'd>(
+    class: &mut CdcAcmClass<'d, Driver<'d, T>>,
+) -> Result<(), Disconnected> {
+    let mut buf = [0; 64];
+    loop {
+        let n = class.read_packet(&mut buf).await?;
+        let data = &buf[..n];
+        info!("data: {:x}", data);
+
+        let mut buf = [0; 256];
+        let mut offset = 0;
+        for c in b"Hello from USB land! You said: '" {
+            buf[offset] = *c;
+            offset += 1;
+        }
+        for c in data.iter() {
+            buf[offset] = *c;
+            offset += 1;
+        }
+        for c in b"'\r\n" {
+            buf[offset] = *c;
+            offset += 1;
+        }
+        let buf = &buf[0..offset];
+        class.write_packet(buf).await?;
+    }
+}
+
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    let p = embassy_stm32::init(Default::default());
+    let mut config = embassy_stm32::Config::default();
+    {
+        use embassy_stm32::rcc::*;
+
+        config.rcc.hsi48 = Some(Hsi48Config {
+            sync_from_usb: true,
+        });
+    }
+
+    let p = embassy_stm32::init(config);
     let mut led = Output::new(p.PB8, Level::High, Speed::Low);
 
-    loop {
-        info!("Hello, World!");
-        led.set_high();
-        Timer::after(Duration::from_millis(500)).await;
-        led.set_low();
-        Timer::after(Duration::from_millis(500)).await;
-    }
+    let driver = Driver::new(p.USB, Irqs, p.PA12, p.PA11);
+
+    let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
+    config.manufacturer = Some("Embassy");
+    config.product = Some("USB-serial example");
+    config.serial_number = Some("12345678");
+
+    // Create embassy-usb DeviceBuilder using the driver and config.
+    // It needs some buffers for building the descriptors.
+    let mut config_descriptor = [0; 256];
+    let mut bos_descriptor = [0; 256];
+    let mut control_buf = [0; 64];
+
+    let mut state = State::new();
+
+    let mut builder = Builder::new(
+        driver,
+        config,
+        &mut config_descriptor,
+        &mut bos_descriptor,
+        &mut [], // no msos descriptors
+        &mut control_buf,
+    );
+
+    // Create classes on the builder.
+    let mut class = CdcAcmClass::new(&mut builder, &mut state, 64);
+
+    // Build the builder.
+    let mut usb = builder.build();
+
+    // Run the USB device.
+    let usb_fut = usb.run();
+
+    // Do stuff with the class!
+    let echo_fut = async {
+        loop {
+            class.wait_connection().await;
+            info!("Connected");
+            let _ = echo(&mut class).await;
+            info!("Disconnected");
+        }
+    };
+
+    let led_fut = async {
+        loop {
+            info!("Hello, World!");
+            led.set_high();
+            Timer::after(Duration::from_millis(500)).await;
+            led.set_low();
+            Timer::after(Duration::from_millis(500)).await;
+        }
+    };
+
+    // Run everything concurrently.
+    // If we had made everything `'static` above instead, we could do this using separate tasks instead.
+    join3(usb_fut, echo_fut, led_fut).await;
 }
